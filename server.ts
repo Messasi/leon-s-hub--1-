14cf02357d762f1b2232dcc28426c5ab66ad5643
leon <leonnupa8@gmail.com>
2026-04-14 03:50:49 +0100

    changed

--- server.ts
+++ server.ts
@@ -42,7 +42,7 @@ async function startServer() {
   const app = express();
   const PORT = 3000;
 
-  app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+  app.use(cors({origin: process.env.CORS_ORIGIN || 'http://localhost:5173', credentials: true}));
   app.use(express.json());
   app.use(cookieParser());
 
